d1cb4516416cbf40af925858e14d6aa59919b5a0
hojiii <shimn3060@naver.com>
2023-11-26 23:02:03 +0900

    캐러쉘 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,14 @@
-import Footer from "../../components/Footer"
-import Header from "../../components/Header"
-import PostList from "../../components/PostList"
+import Carousel from "components/Carousel"
+import Footer from "components/Footer"
+import Header from "components/Header"
+import PostList from "components/PostList"
 
 
 export default function Home(){
   return (
     <div>
       <Header/>
+      <Carousel/>
       <PostList/>
           <Footer/>
     </div>
